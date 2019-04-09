59d429e3af93c4b672b86d879aad442059ab5be7
melaniehall12 <swimdawg@live.com>
2019-04-09 11:36:26 -0400

    Adjusted descrip. again.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 /*
-"A Sweet Visualization"
-CS 4460: P5
+Title: "A Sweet Visualization"
 Group: Chianne Connelly () and Melanie Hall (903112239)
-Data Set: 'candy.csv' 
+CS 4460: P5
+Dataset: 'candy.csv' 
 Categories: Gender, Age, Country, Candies w/ JOY, MEH, or DESPAIR.
 */
+
+
 

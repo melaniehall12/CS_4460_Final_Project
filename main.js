b6da6a70e1db41ba2cbd9b1fc42a82cb89852719
melaniehall12 <swimdawg@live.com>
2019-04-13 10:52:52 -0400

    Pulled in data.

--- main.js
+++ main.js
@@ -5,3 +5,47 @@ CS 4460: P5
 Dataset: 'colleges.csv' 
 */
 
+d3.csv('./data/colleges.csv', function(error, dataset) {
+	console.log(dataset[0])
+    var svgLines = d3.select('svg');
+
+    var margin = {top:50, bottom:50, left:30, right:30};
+
+    var name = d3.nest()
+    	.key(function(d) { return d.Name})
+    	.entries(dataset);
+
+    var control = d3.nest()
+    	.key(function(d) { return d.Control})
+    	.entries(dataset);
+
+    var region = d3.nest()
+    	.key(function(d) { return d.Region})
+    	.entries(dataset);
+
+    var act = d3.nest()
+    	.key(function(d) { return d.ACTMed})
+    	.entries(dataset);
+
+    var sat = d3.nest()
+    	.key(function(d) { return d.SATAverage})
+    	.entries(dataset);			
+
+    var avgCost = d3.nest()
+    	.key(function(d) { return d.AverageCost})
+    	.entries(dataset);
+
+    var debt = d3.nest()
+    	.key(function(d) { return d.Debt})
+    	.entries(dataset);	
+
+    var retenRate = d3.nest()
+    	.key(function(d) { return d.RetentionRate})
+    	.entries(dataset);
+
+    var mean8 = d3.nest()
+    	.key(function(d) { return d.Mean8})
+    	.entries(dataset);	
+
+
+
